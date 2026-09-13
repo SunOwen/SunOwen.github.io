c593d5bf8c6d6f9010ef736d6f218053b6e53eba
SunOwen <35296481+SunOwen@users.noreply.github.com>
2026-09-13 22:00:57 +0800

    refactor: drop Bluesky-specific code from views, utils and project listing

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,7 +4,6 @@ import { defineCollection } from 'astro:content'
 import { feedLoader } from '@ascorbic/feed-loader'
 import { githubReleasesLoader } from 'astro-loader-github-releases'
 import { githubPrsLoader } from 'astro-loader-github-prs'
-import { blueskyPostsLoader } from 'astro-loader-bluesky-posts'
 
 import {
   pageSchema,
@@ -68,28 +67,6 @@ const prs = defineCollection({
   }),
 })
 
-const highlights = defineCollection({
-  loader: blueskyPostsLoader({
-    uris: [
-      'at://did:plc:6kf6jxl44h34mprhykvqljcx/app.bsky.feed.post/3lifesehhok27',
-      'at://did:plc:iwhvwluesbbqtslwwdzgiize/app.bsky.feed.post/3lh3aonbqes2y',
-      'at://did:plc:6kf6jxl44h34mprhykvqljcx/app.bsky.feed.post/3lfwu3pka2c2j',
-      'at://did:plc:6kf6jxl44h34mprhykvqljcx/app.bsky.feed.post/3lfsayyhu4c2j',
-      'at://did:plc:6kf6jxl44h34mprhykvqljcx/app.bsky.feed.post/3lf3iyptedc2e',
-      'at://did:plc:6kf6jxl44h34mprhykvqljcx/app.bsky.feed.post/3lcv2yftszs2z',
-      'at://did:plc:6kf6jxl44h34mprhykvqljcx/app.bsky.feed.post/3lcl5ndm52c2s',
-      'at://did:plc:6kf6jxl44h34mprhykvqljcx/app.bsky.feed.post/3lcdimk36e226',
-      'at://did:plc:6kf6jxl44h34mprhykvqljcx/app.bsky.feed.post/3lbkb6hizhk2f',
-      'at://did:plc:oky5czdrnfjpqslww2a5iclo/app.bsky.feed.post/3lbayyemhzs2v',
-      'at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.post/3larljiyi7s2v',
-    ],
-    newlineHandling: 'paragraph',
-    fetchThread: true,
-    threadDepth: 4,
-    fetchOnlyAuthorReplies: true,
-  }),
-})
-
 const photos = defineCollection({
   loader: file('src/content/photos/data.json'),
   schema: photoSchema,
@@ -127,7 +104,6 @@ export const collections = {
   projects,
   releases,
   prs,
-  highlights,
   photos,
   shorts,
   changelog,
